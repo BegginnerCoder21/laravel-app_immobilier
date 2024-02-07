3ad380df54246fb36683236a1760c82dac45c838
Teguera <aboubacarteguera21@example.com>
2024-02-07 14:47:39 +0000

    gestion du controle d'accès de l'admin auth à login

--- routes/admin.php
+++ routes/admin.php
@@ -4,10 +4,14 @@ use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\LoginController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::prefix('admin')->group(function () {
+Route::prefix('admin')->middleware('auth:admin')->group(function(){
 
     Route::get('/', [AdminController::class, 'index'])->name('admin.dashboard');
+});
+
+Route::prefix('admin')->middleware('guest:admin')->group(function () {
+
+
     Route::get('/login', [LoginController::class, 'getLogin'])->name('get.admin.login');
     Route::post('/login',[LoginController::class,'login'])->name('admin.login');
 });
