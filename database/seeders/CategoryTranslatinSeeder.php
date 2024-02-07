b70076eb89c14fe3c661cd3dbc737ee3a003c524
Teguera <aboubacarteguera21@example.com>
2024-02-07 23:26:21 +0000

    creation de la table category et ajout des champs

---
+++ database/seeders/CategoryTranslatinSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategoryTranslatinSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
