4279ecba16466c500c36f2880493adb75ab4e80f
Teguera <aboubacarteguera21@example.com>
2024-02-07 23:37:07 +0000

    creation de la table category translation et configuration du model

--- database/migrations/2024_02_07_232112_create_category_translatins_table.php
+++ database/migrations/2024_02_07_232112_create_category_translatins_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('category_translatins', function (Blueprint $table) {
             $table->id();
+            $table->string('locale')->unique();
+            $table->string('name');
+            $table->foreignIdFor(\App\Models\Category::class);
             $table->timestamps();
         });
     }
