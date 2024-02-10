cdeb74af91566fc489795ba53f0ab532862bc6bd
Teguera <aboubacarteguera21@example.com>
2024-02-10 23:10:16 +0000

    mise a jour de la category implementé

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -39,5 +39,36 @@ class MainController extends Controller
 
     }
 
+    public function update($id, UpdateCategoryRequest $request){
+
+        try {
+            DB::beginTransaction();
+            $category = Category::findOrFail($id);
+
+            if(!$category){
+                return view('admin.categories')->with(['error' => "Category non trouvé"]);
+            }
+
+            if (!$request->has('is_active')) {
+                $request->merge(['is_active' => 0]);
+            } else {
+                $request->merge(['is_active' => 1]);
+            }
+
+            $category->update([
+                ...$request->validated(),
+                'is_active' => $request->is_active
+            ]);
+
+            DB::commit();
+
+            return redirect()->route('admin.categories')->with(["succes" => "La category a été mis à jour"]);
+        }catch (\Exception $error){
+            DB::rollBack();
+            return redirect()->route('admin.categories')->with(["error" => "Quelque chose s'est mal passé"]);
+        }
+
+
+    }
 
 }
