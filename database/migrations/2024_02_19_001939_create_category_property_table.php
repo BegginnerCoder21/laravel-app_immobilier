62302e4fa47f6baa0cb5e3bad34ede5752070b9d
Teguera <aboubacarteguera21@example.com>
2024-02-19 00:35:32 +0000

    creation de la classe d'association entre property et category

--- database/migrations/2024_02_19_001939_create_category_property_table.php
+++ database/migrations/2024_02_19_001939_create_category_property_table.php
@@ -12,7 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('category_property', function (Blueprint $table) {
-            $table->id();
+            $table->integer('property_id')->unsigned();
+            $table->integer('category_id')->unsigned();
+
+            $table->primary(['property_id', 'category_id']);
+            $table->foreign('property_id')->references('id')->on('properties')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
             $table->timestamps();
         });
     }
