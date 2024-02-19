3dfa4dc6a3036bae19909734b458c246327513f8
Teguera <aboubacarteguera21@example.com>
2024-02-19 00:20:54 +0000

    mise en place des routes pour la gestion des propriétés

--- routes/admin.php
+++ routes/admin.php
@@ -15,69 +15,83 @@ use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 Route::group(
     [
         'prefix' => LaravelLocalization::setLocale(),
-        'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
-    ], function(){
+        'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']
+    ],
+    function () {
 
-Route::prefix('admin')->middleware('auth:admin')->group(function(){
+        Route::prefix('admin')->middleware('auth:admin')->group(function () {
 
-    Route::get('/', [AdminController::class, 'index'])->name('admin.dashboard');
+            Route::get('/', [AdminController::class, 'index'])->name('admin.dashboard');
 
-    Route::prefix('main_categories')->group(function(){
-        
-        Route::controller(MainCategoryController::class)->group(function (){
-            Route::get('/','index')->name('admin.categories');
-            Route::get('create','create')->name('admin.categories.create');
-            Route::get('edit/{id}','edit')->name('admin.categories.edit');
-            Route::get('delete/{id}','delete')->name('admin.categories.delete');
+            Route::prefix('main_categories')->group(function () {
+
+                Route::controller(MainCategoryController::class)->group(function () {
+                    Route::get('/', 'index')->name('admin.categories');
+                    Route::get('create', 'create')->name('admin.categories.create');
+                    Route::get('edit/{id}', 'edit')->name('admin.categories.edit');
+                    Route::get('delete/{id}', 'delete')->name('admin.categories.delete');
+                });
+                Route::middleware('isActive')->group(function () {
+                    Route::post('store', [StoreCategoryController::class, 'store'])->name('admin.categories.store');
+                    Route::post('update/{id}', [UpdateCategoryController::class, 'update'])->name('admin.categories.update');
+                });
+            });
+
+            Route::prefix('cities')->group(function () {
+
+                Route::controller(MainCityController::class)->group(function () {
+                    Route::get('/', 'index')->name('admin.cities');
+                    Route::get('create', 'create')->name('admin.cities.create');
+                    Route::get('edit/{id}', 'edit')->name('admin.cities.edit');
+                    Route::get('delete/{id}', 'delete')->name('admin.cities.delete');
+                });
+                Route::middleware('isActive')->group(function () {
+
+                    Route::post('store', [StoreCityController::class, 'store'])->name('admin.cities.store');
+                    Route::post('update/{id}', [UpdateCityController::class, 'update'])->name('admin.cities.update');
+                });
+            });
+
+            Route::prefix('user')->group(function () {
+
+                Route::controller(MainUserController::class)->group(function () {
+                    Route::get('/', 'index')->name('admin.users');
+                    Route::get('create', 'create')->name('admin.users.create');
+                    Route::post('store', 'store')->name('admin.users.store');
+                    Route::get('delete/{id}', 'delete')->name('admin.users.delete');
+                });
+            });
+
+            Route::prefix('properties')->group(function () {
+
+                Route::controller(MainUserController::class)->group(function () {
+                    Route::get('/', 'index')->name('admin.properties');
+                    Route::get('create', 'create')->name('admin.properties.general.create');
+                    Route::post('store', 'store')->name('admin.properties.general.store');
+                    Route::get('edit/{id}', 'edit')->name('admin.properties.edit');
+                    Route::post('update/{id}', 'update')->name('admin.properties.update');
+                    Route::get('delete/{id}', 'delete')->name('admin.properties.delete');
+
+                    Route::post('price', 'getPrice')->name('admin.properties.price');
+                    Route::get('price/{id}', 'savePropertyPrice')->name('admin.properties.price.create');
+
+                    Route::post('feature', 'getFeature')->name('admin.properties.features');
+                    Route::get('feature/{id}', 'savePropertyFeature')->name('admin.properties.features.create');
+
+                    Route::get('image/{id}', 'addImages')->name('admin.properties.images');
+                    Route::post('image', 'savePropertyImages')->name('admin.properties.images.store');
+                    Route::post('image/db', 'savePropertyImagesDB')->name('admin.properties.images.store.db');
+                });
+            });
         });
-        Route::middleware('isActive')->group(function(){
-            Route::post('store',[StoreCategoryController::class,'store'])->name('admin.categories.store');
-            Route::post('update/{id}',[UpdateCategoryController::class,'update'])->name('admin.categories.update');
 
+        Route::prefix('admin')->middleware('guest:admin')->group(function () {
+
+            Route::controller(LoginController::class)->group(function () {
+
+                Route::get('/login', 'getLogin')->name('get.admin.login');
+                Route::post('/login', 'login')->name('admin.login');
+            });
         });
-
-    });
-
-    Route::prefix('cities')->group(function(){
-
-        Route::controller(MainCityController::class)->group(function (){
-            Route::get('/','index')->name('admin.cities');
-            Route::get('create','create')->name('admin.cities.create');
-            Route::get('edit/{id}','edit')->name('admin.cities.edit');
-            Route::get('delete/{id}','delete')->name('admin.cities.delete');
-        });
-        Route::middleware('isActive')->group(function() {
-
-            Route::post('store',[StoreCityController::class,'store'])->name('admin.cities.store');
-            Route::post('update/{id}',[UpdateCityController::class,'update'])->name('admin.cities.update');
-        });
-
-    });
-
-    Route::prefix('user')->group(function(){
-
-        Route::controller(MainUserController::class)->group(function (){
-            Route::get('/','index')->name('admin.users');
-            Route::get('create','create')->name('admin.users.create');
-            Route::post('store','store')->name('admin.users.store');
-            Route::get('delete/{id}','delete')->name('admin.users.delete');
-        });
-    
-
-
-    });
-
-    
-});
-
-Route::prefix('admin')->middleware('guest:admin')->group(function () {
-
-    Route::controller(LoginController::class)->group(function(){
-
-        Route::get('/login', 'getLogin')->name('get.admin.login');
-        Route::post('/login', 'login')->name('admin.login');
-
-    });
-});
-
-});
+    }
+);
